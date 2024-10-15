e184f950d4e3c454c8aeab53478f2738b19e9dfc
highskore <luka@racki.dev>
2024-10-15 12:05:23 +0200

    chore: lint

--- test/utils/TestBaseUtil.sol
+++ test/utils/TestBaseUtil.sol
@@ -24,9 +24,7 @@ contract TestBaseUtil is Test {
         factory = new MockFactory(address(implementation));
     }
 
-    function getAccountAndInitCode(
-        bytes32 salt
-    )
+    function getAccountAndInitCode(bytes32 salt)
         internal
         returns (address account, bytes memory initCode)
     {
--- test/utils/TestBaseUtilV060.sol
+++ test/utils/TestBaseUtilV060.sol
@@ -27,9 +27,7 @@ contract TestBaseUtil is Test {
         factory = new MockFactory(address(implementation));
     }
 
-    function getAccountAndInitCode(
-        bytes32 salt
-    )
+    function getAccountAndInitCode(bytes32 salt)
         internal
         returns (address account, bytes memory initCode)
     {
